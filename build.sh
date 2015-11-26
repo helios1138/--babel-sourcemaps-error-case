4201c0d1b301d30b1c63b3ba68612c3ec495154e
helios_m <revan.den@gmail.com>
2015-11-26 13:43:44 +0200

    init

--- build.sh
+++ build.sh
@@ -8,6 +8,5 @@ errors=$(./node_modules/typescript/bin/tsc)
 if [[ $? != 0 ]]; then
     echo "$errors"
 else
-    ./node_modules/typescript/bin/tsc
     ./node_modules/babel-cli/bin/babel.js pre-build/ --out-dir build/ --source-maps inline
 fi
